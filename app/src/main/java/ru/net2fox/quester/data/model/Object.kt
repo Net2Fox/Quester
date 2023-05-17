8356b6a88a4463b1cdafc44e2b4d65126b2561db
Net2Fox <trakounet.fox@gmail.com>
2023-05-18 00:36:21 +0300

    Добавлена строка объекта

--- app/src/main/java/ru/net2fox/quester/data/model/Object.kt
+++ app/src/main/java/ru/net2fox/quester/data/model/Object.kt
@@ -1,8 +1,16 @@
 package ru.net2fox.quester.data.model
 
-enum class Object {
-    LIST,
-    TASK,
-    SKILL,
-    ACCOUNT
+import androidx.annotation.StringRes
+import ru.net2fox.quester.Quester
+import ru.net2fox.quester.R
+
+enum class Object(@StringRes val nameString: Int) {
+    LIST(R.string.enum_object_list),
+    TASK(R.string.enum_object_task),
+    SKILL(R.string.enum_object_skill),
+    ACCOUNT(R.string.enum_object_account);
+
+    override fun toString(): String {
+        return Quester.getContext().resources.getString(this.nameString)
+    }
 }
