fe3fb68ae3cad3a239ca695f0141aa1f20803dab
Net2Fox <trakounet.fox@gmail.com>
2023-05-18 00:34:31 +0300

    Добавлена строка действия

--- app/src/main/java/ru/net2fox/quester/data/model/Action.kt
+++ app/src/main/java/ru/net2fox/quester/data/model/Action.kt
@@ -1,8 +1,16 @@
 package ru.net2fox.quester.data.model
 
-enum class Action {
-    CREATE,
-    EDIT,
-    DELETE,
-    MARK_COMPLETE
+import androidx.annotation.StringRes
+import ru.net2fox.quester.Quester
+import ru.net2fox.quester.R
+
+enum class Action(@StringRes val descriptionString: Int) {
+    CREATE(R.string.enum_action_create),
+    EDIT(R.string.enum_action_edit),
+    DELETE(R.string.enum_action_delete),
+    MARK_COMPLETE(R.string.enum_action_mark_complete);
+
+    override fun toString(): String {
+        return Quester.getContext().resources.getString(this.descriptionString)
+    }
 }
